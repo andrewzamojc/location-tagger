a4cbca263e08ae7a333efcd8cfe97f18f7646667
Andrew Zamojc <andrewzamojc@gmail.com>
2016-01-04 18:03:50 -0500

    add angular to the project

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ var express    = require('express');
 var app    = express();
 var port   = process.env.PORT || 8080;
 
+// static files
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+// jade views
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+// routes
 app.get('/', function(req, res) {
     res.render('index');
 });
 
+// start the server
 app.listen(port);
 console.log('server running on port ' + port);
