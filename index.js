5672f01e303df523cf96fbf33ba0d2914828c8b5
Andrew Zamojc <andrewzamojc@gmail.com>
2016-01-04 17:49:08 -0500

    add the first jade view

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var express    = require('express');
 var app    = express();
 var port   = process.env.PORT || 8080;
 
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res) {
-    res.json({ message: 'Welcome to Location Tagger' });
+    res.render('index');
 });
 
 app.listen(port);
